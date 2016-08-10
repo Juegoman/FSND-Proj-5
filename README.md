33a03a4b3ed3f890947808943f3c2a6bcf7c4449
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-08-10 04:20:44 -0700

    initial commit, ready for submit

---
+++ README.md
@@ -0,0 +1,17 @@
+Udacity Full Stack Nanodegree Project 5: Item Catalog
+-----------------------------------------------------
+A website where you can view restaurants and their menus.
+Users can log in using social media accounts and create new restaurants and menus.
+
+To run this application python 2.7 is required.
+Along with python, the following dependencies are required:
+
+- python-flask
+- python-sqlalchemy
+- oauth2client
+- requests
+- httplib2
+
+install all dependencies using your operating system's specific methods.
+
+After dependencies are installed, running database_setup.py followed by final_project.py will launch the server locally at http://localhost:5000.
---
+++ database_setup.py
@@ -0,0 +1,70 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User table definition"""
+    __tablename__ = 'user'
+
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class Restaurant(Base):
+    """Restaurant table definition"""
+    __tablename__ = 'restaurant'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Returns object data in easily serializable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class MenuItem(Base):
+    """MenuItem table definition"""
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Returns object data in easily serializable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
